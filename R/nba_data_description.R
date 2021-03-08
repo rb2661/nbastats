08f64d5807844d8282e5caa41e3270749a99a44e
Rory Butler <rb2661@columbia.edu>
2021-03-08 17:35:00 -0500

    Create nba_data_description.R

---
+++ R/nba_data_description.R
@@ -0,0 +1,11 @@
+#' NBA Season Stats for Players
+#'
+#' A dataset containing the stats of NBA players across several years
+#'
+#' @format A data frame with 24691 rows and 52 variables:
+#' \describe{
+#'   \item{Year}{Year representing the NBA season}
+#'   \item{Player}{Name of player}
+#'   ...
+#' }
+"nba"
