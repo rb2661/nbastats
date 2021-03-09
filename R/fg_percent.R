eb53651b38d2cb23b50e99a8e3f94f119392ccc2
Rory Butler <rb2661@columbia.edu>
2021-03-08 20:16:04 -0500

    added input data variable

--- R/fg_percent.R
+++ R/fg_percent.R
@@ -1,7 +1,7 @@
 #' fg_percent function
 #'
 #' This function allows you to check Michael Jordan's field goal percentage in a given year.
-#' @param year What year are you interested in? Defaults to 1990.
+#' @param input_data, year What year are you interested in? Defaults to 1990.
 #' @keywords FG
 #' @export
 #' @import dplyr
@@ -10,9 +10,9 @@
 #' fg_percent()
 
 
-fg_percent <- function(year=1990) {
+fg_percent <- function(input_data=nba, year=1990) {
   library(magrittr, "%>%")
-  nba %>%
+  input_data %>%
     dplyr::select(Player, Year, `FG%`) %>%
     dplyr::filter(Year == year, Player == 'Michael Jordan*')
 }
